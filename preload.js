b1445deb0a278f41bf10e1c0e107f6bcf6ecc58b
Alejandro R. <alrolja@gmail.com>
2025-03-24 00:45:59 -0400

    clipboard api working on

--- preload.js
+++ preload.js
@@ -1,5 +1,11 @@
 const { ipcRenderer, contextBridge } = require('electron');
+const { contextBridge, clipboard } = require('electron');
+
 
 contextBridge.exposeInMainWorld('electronAPI', {
   navigate: (url) => ipcRenderer.send('navigate', url)
+});
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  copyText: (text) => clipboard.writeText(text)
 });
