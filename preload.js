59547af50b5014bc12b90d8e1add06b112a022b4
Alejandro R. <alrolja@gmail.com>
2025-03-24 20:05:57 -0400

    added icon and fixed preload js

--- preload.js
+++ preload.js
@@ -1,11 +1,7 @@
 const { ipcRenderer, contextBridge } = require('electron');
 const { contextBridge, clipboard } = require('electron');
 
-
 contextBridge.exposeInMainWorld('electronAPI', {
-  navigate: (url) => ipcRenderer.send('navigate', url)
-});
-
-contextBridge.exposeInMainWorld('electronAPI', {
+  navigate: (url) => ipcRenderer.send('navigate', url),
   copyText: (text) => clipboard.writeText(text)
 });
