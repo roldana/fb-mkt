0f8aecf4eb71380ad88a0e48a2700632d2099fdb
roldana <alrolja@gmail.com>
2026-08-16 22:28:36 -0400

    complete app revamp

--- preload.js
+++ preload.js
@@ -1,15 +1,6 @@
 const { ipcRenderer, contextBridge } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  navigate: (url) => ipcRenderer.send('navigate', url),
-  saveSearch: (query) => ipcRenderer.send('save-search', query),
-  requestSavedSearches: () => ipcRenderer.send('get-saved-searches'),
-  onSearchSaved: (callback) =>
-    ipcRenderer.on('search-saved', (event, data) => callback(data)),
-  onReceiveSavedSearches: (callback) =>
-    ipcRenderer.on('saved-searches', (event, data) => callback(data)),
-  copyText: (text) => ipcRenderer.send('copy-to-clipboard', text),
-  toggleSidebar: (collapsed) => 
-    ipcRenderer.send('sidebar-toggled', collapsed)
-  
+  copyUrl: (url) => ipcRenderer.invoke('copy-url', url)
 });
+    
